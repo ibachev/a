1c6c4e912e7bf6547e269bebbffa86c10f218f55
Igor Bachev <igor.bachev95@gmail.com>
2026-01-16 20:41:09 +0100

    go

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,14 +11,9 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000000",
     icons: [
       {
-        src: "/icon-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/icon-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
+        src: "/next.svg", // SVG version
+        sizes: "any", // for SVG you can put 'any'
+        type: "image/svg+xml",
       },
     ],
   };
