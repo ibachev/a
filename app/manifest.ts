68fadd4af952cdb71b312a3dbbe625ae41ec1f8b
Igor Bachev <igor.bachev95@gmail.com>
2026-01-16 20:58:11 +0100

    goo

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000000",
     icons: [
       {
-        src: "/image.png",
+        src: "/logo.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/image.png",
+        src: "/logo.png",
         sizes: "512x512",
         type: "image/png",
       },
